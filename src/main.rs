37383d25f5b3dbf9e81ce4a9d1225788f6470d6c
richod1 <frimpsup@gmail.com>
2023-06-23 13:34:28 -0400

    first rust app

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
-    println!("Hello, welcome to guess game in rust");
-    println!("pls input your guess");
 
-    let mut guess=String::new();
-    io::stdin().read_line(&mut guess).expect("failed to read");
+    let random_number = rand::thread_rng().gen_range(1..=100);
+    
+    println!("Random number: {}", random_number);
+    // println!("Hello, welcome to guess game in rust");
+    // println!("pls input your guess");
 
-    println!("you guess : {}",guess);
+    // let secret_number=rand::thread_rng().gen_range(1,101);
+
+    // println!("secreate gen number is :{}",secret_number);
+
+    // let mut guess=String::new();
+    // io::stdin().read_line(&mut guess).expect("failed to read");
+
+    // println!("you guess : {}",guess);
+
+    // // generating random numbers for 1 to 100
+
+    
 }
